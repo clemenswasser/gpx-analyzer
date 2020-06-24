6b48976fb20bd59387426d605768f223ca5c7a47
Clemens Wasser <clemens.wasser@gmail.com>
2020-06-24 20:15:06 +0200

    Added support for time parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::prelude::*;
 use geo::prelude::*;
+use rayon::prelude::*;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -12,27 +14,30 @@ struct Opt {
     pub coordinate: Option<String>,
     #[structopt(short, long)]
     pub distance: f64,
-    #[structopt(short, long)]
-    pub path: Option<std::path::PathBuf>,
     #[structopt(short = "j", long)]
     pub threads: Option<usize>,
+    #[structopt(name = "PATH")]
+    pub path: Option<std::path::PathBuf>,
 }
 
-struct Result {
+struct GpxResult {
     distance: f64,
     path: String,
     time: Option<String>,
 }
 
-fn analyze(path: &std::path::PathBuf, lon: f64, lat: f64, distance: f64) -> Vec<Result> {
+fn analyze(path: &std::path::PathBuf, lon: f64, lat: f64, distance: f64) -> Vec<GpxResult> {
     let mut reader = quick_xml::Reader::from_file(&path).unwrap();
     reader.trim_text(true);
     let mut buf = Vec::new();
 
-    let mut results = Vec::new();
-    let mut nearest: Option<Result> = None;
+    let mut results: Vec<GpxResult> = Vec::new();
+    let mut new_results: Vec<GpxResult> = Vec::new();
+    let mut nearest: Option<GpxResult> = None;
     let mut time_update = false;
+    let mut in_time = false;
     let mut searching_time_for = std::usize::MAX;
+    let mut last_point = None;
 
     loop {
         match reader.read_event(&mut buf) {
@@ -43,34 +48,96 @@ fn analyze(path: &std::path::PathBuf, lon: f64, lat: f64, distance: f64) -> Vec<
                             Ok(attr) => attr.key == b"lon",
                             Err(_) => false,
                         }) {
-                            Some(attr) => attr
-                                .unwrap()
+                            Some(attr) => {
+                                match match match attr {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "[WARNING] Invalid longitude in file: {}",
+                                            path.to_str().unwrap()
+                                        );
+                                        continue;
+                                    }
+                                }
                                 .unescape_and_decode_value(&reader)
-                                .unwrap()
+                                {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "[WARNING] Invalid longitude in file: {}",
+                                            path.to_str().unwrap()
+                                        );
+                                        continue;
+                                    }
+                                }
                                 .parse::<f64>()
-                                .unwrap(),
+                                {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "[WARNING] Invalid longitude in file: {}",
+                                            path.to_str().unwrap()
+                                        );
+                                        continue;
+                                    }
+                                }
+                            }
                             None => continue,
                         },
                         match e.attributes().find(|e| match e {
                             Ok(attr) => attr.key == b"lat",
                             Err(_) => false,
                         }) {
-                            Some(attr) => attr
-                                .unwrap()
+                            Some(attr) => {
+                                match match match attr {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "[WARNING] Invalid latitude in file: {}",
+                                            path.to_str().unwrap()
+                                        );
+                                        continue;
+                                    }
+                                }
                                 .unescape_and_decode_value(&reader)
-                                .unwrap()
+                                {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "[WARNING] Invalid latitude in file: {}",
+                                            path.to_str().unwrap()
+                                        );
+                                        continue;
+                                    }
+                                }
                                 .parse::<f64>()
-                                .unwrap(),
+                                {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "[WARNING] Invalid latitude in file: {}",
+                                            path.to_str().unwrap()
+                                        );
+                                        continue;
+                                    }
+                                }
+                            }
                             None => continue,
                         },
                     );
 
-                    let dist = geo_types::Point::new(lon, lat).haversine_distance(&pt);
+                    let dist = match last_point {
+                        Some(last_point) => {
+                            let line = geo_types::Line::new(last_point, pt);
+                            geo_types::Point::new(lon, lat).euclidean_distance(&line)
+                        }
+                        None => geo_types::Point::new(lon, lat).haversine_distance(&pt),
+                    };
 
                     if (nearest.is_none() || nearest.as_ref().unwrap().distance > dist)
                         && dist > distance
                     {
-                        nearest = Some(Result {
+                        nearest = Some(GpxResult {
                             distance: dist,
                             path: path.to_str().unwrap().to_string(),
                             time: None,
@@ -78,18 +145,40 @@ fn analyze(path: &std::path::PathBuf, lon: f64, lat: f64, distance: f64) -> Vec<
                         time_update = true;
                     }
 
+                    if dist > distance && !new_results.is_empty() {
+                        new_results.sort_by(|result_1: &GpxResult, result_2: &GpxResult| {
+                            result_1.distance.partial_cmp(&result_2.distance).unwrap()
+                        });
+                        results.push(new_results.remove(0));
+                        new_results.clear();
+                    }
+
                     if dist <= distance {
-                        results.push(Result {
+                        new_results.push(GpxResult {
                             distance: dist,
                             path: path.to_str().unwrap().to_string(),
                             time: None,
                         });
-                        searching_time_for = results.len() - 1;
+                        searching_time_for = new_results.len() - 1;
                     }
+                    last_point = Some(pt);
                 } else if e.name().eq(b"time") {
-                    let time = reader.read_text(e.name(), &mut Vec::new()).unwrap();
+                    in_time = true;
+                }
+            }
+            Ok(quick_xml::events::Event::End(e)) => {
+                if e.name().eq(b"trkpt") {
+                    searching_time_for = std::usize::MAX;
+                }
+                if e.name().eq(b"time") {
+                    in_time = false;
+                }
+            }
+            Ok(quick_xml::events::Event::Text(e)) => {
+                if in_time {
+                    let time = e.unescape_and_decode(&reader).unwrap();
                     if !time.eq("") {
-                        if let Some(time_for) = results.get_mut(searching_time_for) {
+                        if let Some(time_for) = new_results.get_mut(searching_time_for) {
                             time_for.time = Some(time);
                         } else if time_update {
                             nearest.as_mut().unwrap().time = Some(time);
@@ -98,30 +187,37 @@ fn analyze(path: &std::path::PathBuf, lon: f64, lat: f64, distance: f64) -> Vec<
                     time_update = false;
                 }
             }
-            Ok(quick_xml::events::Event::End(e)) => {
-                if e.name().eq(b"trkpt") {
-                    searching_time_for = std::usize::MAX;
-                }
-            }
-            Ok(quick_xml::events::Event::Text(_)) => {}
             Ok(quick_xml::events::Event::Eof) => {
-                if results.len() > 0 {
-                    if let Some(nearest) = nearest {
-                        results.push(nearest);
-                    }
-                    return results;
-                } else if let Some(nearest) = nearest {
-                    return vec![nearest];
+                let capacity = if nearest.is_some() {
+                    results.len() + std::cmp::min(new_results.len(), 1) + 1
                 } else {
-                    eprintln!(
-                        "[Warning] No points in file: \"{}\"",
-                        path.to_str().unwrap()
-                    );
+                    results.len() + std::cmp::min(new_results.len(), 1)
+                };
+                if capacity == 0 {
                     return vec![];
                 }
+                let mut out = Vec::<GpxResult>::with_capacity(capacity);
+
+                if results.is_empty() && new_results.is_empty() {
+                    if let Some(nearest) = nearest.take() {
+                        out.push(nearest);
+                    }
+                }
+
+                if !results.is_empty() {
+                    out.extend(results);
+                }
+                if !new_results.is_empty() {
+                    new_results.sort_by(|result_1: &GpxResult, result_2: &GpxResult| {
+                        result_1.distance.partial_cmp(&result_2.distance).unwrap()
+                    });
+                    out.push(new_results.remove(0));
+                }
+
+                return out;
             }
-            Err(e) => panic!(
-                "Error in file \"{}\", at position {}: {:?}",
+            Err(e) => eprintln!(
+                "[Error] file: \"{}\"; position: {}; {:?}",
                 path.to_str().unwrap(),
                 reader.buffer_position(),
                 e
@@ -130,8 +226,6 @@ fn analyze(path: &std::path::PathBuf, lon: f64, lat: f64, distance: f64) -> Vec<
         }
     }
 }
-
-use rayon::prelude::*;
 
 fn read_dir_db(path: std::path::PathBuf) -> Vec<std::path::PathBuf> {
     if std::fs::metadata(&path).unwrap().is_dir() {
@@ -155,31 +249,46 @@ fn read_dir_db(path: std::path::PathBuf) -> Vec<std::path::PathBuf> {
     }
 }
 
-fn parse_deg_min_sec(input: String) -> f64 {
-    let mut split = input
-        .split(" ")
+fn parse_deg_min_sec(mut input: String) -> f64 {
+    input = input.trim().to_string();
+    let first = input.remove(0);
+    let south_west = first.eq(&'S') || first.eq(&'W');
+    input = input.trim().to_string();
+
+    let split = input
+        .split(' ')
         .map(|str| str.to_string())
         .collect::<Vec<_>>();
-    let mut param = split.remove(0);
-    let dir_param = param.remove(0);
-    let south = dir_param.eq(&'S') || dir_param.eq(&'W');
 
-    let mut out =
-        param.parse::<f64>().unwrap() + split.get(0).unwrap().parse::<f64>().unwrap() / 60.0;
-    if south {
+    let mut out = split
+        .get(0)
+        .unwrap()
+        .replace("°", "")
+        .parse::<f64>()
+        .unwrap()
+        + split.get(1).unwrap().parse::<f64>().unwrap() / 60.0;
+    if south_west {
         out *= -1.0;
     };
     out
 }
 
-fn print_result(result: &Result) {
+fn print_result(result: &GpxResult) {
     if result.time.is_some() {
-        let time = result.time.as_ref().unwrap();
-        let mut time_split = time.split("T");
-        let date = time_split.next().unwrap();
-        let mut time = time_split.next().unwrap().to_string();
-        time.remove(time.len() - 1);
-        println!("{:.1};{};{};{}", result.distance, time, date, result.path);
+        let time = result
+            .time
+            .as_ref()
+            .unwrap()
+            .parse::<DateTime<Utc>>()
+            .unwrap()
+            .with_timezone(&chrono::offset::Local);
+        println!(
+            "{:.1};{};{};{}",
+            result.distance,
+            time.time().to_string(),
+            time.date().to_string(),
+            result.path
+        );
     } else {
         println!(
             "{:.1};{};{};{}",
@@ -205,24 +314,44 @@ fn main() {
             )
         } else if let Some(coordinates) = opt.coordinate {
             let split = coordinates
-                .split(" ")
+                .split(' ')
                 .map(|split| split.to_string())
                 .collect::<Vec<_>>();
             if let (Ok(latitude), Ok(longitude)) = (
-                split.get(0).unwrap().parse::<f64>(),
-                split.get(1).unwrap().parse::<f64>(),
+                split.get(0).unwrap().replace(",", "").parse::<f64>(),
+                split.get(1).unwrap().replace(",", "").parse::<f64>(),
             ) {
                 (latitude, longitude)
             } else {
-                let first = split.get(0).unwrap().to_string() + " " + split.get(1).unwrap();
-                let second = split.get(2).unwrap().to_string() + " " + split.get(3).unwrap();
-                (parse_deg_min_sec(first), parse_deg_min_sec(second))
+                let (first, second) = coordinates.split_at(coordinates.len() / 2);
+
+                (
+                    parse_deg_min_sec(first.to_string()),
+                    parse_deg_min_sec(second.to_string()),
+                )
             }
         } else {
             panic!("You must specify either --longitude and --latitude or --coordinates");
         };
 
-    dbg!(latitude, longitude);
+    println!("{}, {}", latitude, longitude);
+    println!(
+        "{} {}° {:.6} {} {}° {:.6}",
+        if latitude.is_sign_negative() {
+            "S"
+        } else {
+            "N"
+        },
+        latitude as u64,
+        latitude % 1.0 * 60.0,
+        if latitude.is_sign_negative() {
+            "W"
+        } else {
+            "E"
+        },
+        longitude as u64,
+        longitude % 1.0 * 60.0,
+    );
 
     if opt.threads.is_some() {
         rayon::ThreadPoolBuilder::new()
@@ -251,7 +380,7 @@ fn main() {
             .sum(),
     );
     analysis_results.iter().for_each(|result| {
-        if result.len() > 0 {
+        if !result.is_empty() {
             results.extend(result);
         }
     });
@@ -265,7 +394,7 @@ fn main() {
         .filter(|result| result.distance <= distance)
         .collect::<Vec<_>>();
 
-    if filtered_results.len() > 0 {
+    if !filtered_results.is_empty() {
         println!(
             "Found {} point(s) in your defined minimum distance ({}m):\n\
             dist;time;date;path",
@@ -283,7 +412,7 @@ fn main() {
             println!("Nearest point out of distance was:");
             print_result(result);
         }
-    } else {
+    } else if !results.is_empty() {
         println!(
             "Did not find any point in your defined minimum distance.\n\
             Closest was:\n\
@@ -291,5 +420,9 @@ fn main() {
         );
         std::mem::drop(filtered_results);
         print_result(results.first().unwrap());
+    } else {
+        println!(
+            "Did not find any points."
+        );
     }
 }
